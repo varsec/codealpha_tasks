c292d04479a69405c2266d74297f840a2db86809
VARSHA <119604633+varsec@users.noreply.github.com>
2025-07-02 14:36:40 +0530

    Add files via upload

---
+++ Hangman_output.py
@@ -0,0 +1,17 @@
+Word: _ _ _ _ _ _
+Guess a letter: a
+Correct guess!
+
+Word: _ a _ a _ a
+Guess a letter: e
+Wrong guess! You have 5 tries left.
+
+Word: _ a _ a _ a
+Guess a letter: n
+Correct guess!
+
+Word: _ a n a n a
+Guess a letter: b
+Correct guess!
+
+Congratulations! You guessed the word: banana
---
+++ chatbox output.py
@@ -0,0 +1,7 @@
+Chatbot: Hi there! Type 'bye' to exit.
+You: hello
+Chatbot: Hi!
+You: how are you
+Chatbot: I'm fine, thanks!
+You: bye
+Chatbot: Goodbye!
